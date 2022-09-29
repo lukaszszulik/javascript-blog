88ad2c8bc1d88c1c9f4649fef5d0f7b33af98641
Lukasz Szulik <lukaszgit@o2.pl>
2022-09-29 20:38:41 +0200

    Job done

--- js/script.js
+++ js/script.js
@@ -12,11 +12,12 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
+  event.preventDefault();
   const clickedElement = this;
   console.log('clickedElement:', clickedElement);
-  activeLink.classList.add('active');
+  clickedElement.classList.add('active');
 
 
   /* [DONE] remove class 'active' from all articles */
@@ -27,11 +28,19 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /*  [DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
