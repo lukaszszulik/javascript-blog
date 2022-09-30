d3870d5b994b89752786793594b5589074ddfe90
Lukasz Szulik <lukaszgit@o2.pl>
2022-09-30 19:43:02 +0200

    Add generating links

--- js/script.js
+++ js/script.js
@@ -43,7 +43,58 @@ for(let activeArticle of activeArticles){
   targetArticle.classList.add('active');
 }
 
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* [DONE] remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* [DONE] for each article */
+
+  let html ='';
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+
+
+
+    /* [DONE] get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* [DONE] find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* [DONE] get the title from the title element */
+
+    const linkHTML = '<li><a href="#' + articleId +  '"><span>' + articleTitle +  '</span></a></li>';
+    console.log(linkHTML);
+
+    /* create HTML of the link */
+
+    titleList.insertAdjacentHTML('beforeend', linkHTML);
+
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log(html);
+  }
+
+  titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
