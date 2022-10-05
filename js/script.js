5752a5d07b3bc9bfc40772b7b4bc428139386d1e
Lukasz Szulik <lukaszgit@o2.pl>
2022-10-05 19:13:22 +0200

    get tags from data-tags attribute

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ function titleClickHandler(event){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
+  optArticleTagsSelector = '.post-tags .list'
 
 function generateTitleLinks(){
 
@@ -99,3 +100,41 @@ console.log(links);
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+function generateTags(){
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+
+  for(let article of articles){}
+
+    /* find tags wrapper */
+
+    const titleList = article.querySelector(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+
+    let html='';
+
+    /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
