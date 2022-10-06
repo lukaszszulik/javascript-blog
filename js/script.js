6e49c729d4ed5d25ed98e6a8299f38642d396601
Lukasz Szulik <lukaszgit@o2.pl>
2022-10-06 18:59:59 +0200

    make new constant named clickedElement and give itthe value of this

--- js/script.js
+++ js/script.js
@@ -160,6 +160,9 @@ function tagClickHandler(event){
 
   /* make new constant named "clickedElement" and give it the value of "this" */
 
+  const clickedElement = this;
+  console.log('Tag was clicked');
+
   /* make a new constant "href" and read the attribute "href" of the clicked element */
 
   /* make a new constant "tag" and extract tag from the "href" constant */
