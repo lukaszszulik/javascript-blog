a9c3aa6a7932c7c9947e2239fd9b2884d79cdd7b
Lukasz Szulik <lukaszgit@o2.pl>
2022-10-12 13:48:31 +0200

    adding a space

--- js/script.js
+++ js/script.js
@@ -240,7 +240,7 @@ function tagClickHandler(event){
 
   /* make a new constant "tag" and extract tag from the "href" constant */
 
-  const tag = href.replace('#tag-','');
+  const tag = href.replace('#tag-', '');
 
   /* [DONE] find all tag links with class active */
 
